03faac8d50079e308fbc1acb0fb09fb63587d7bb
Abdallah Ayad <abdallahayad23@gmail.com>
2021-09-25 12:42:25 +0200

    Added firebase config to env

--- util/firebase.js
+++ util/firebase.js
@@ -1,9 +1,16 @@
 const firebase = require("firebase/app");
 const path = require("path");
 const { getStorage, ref, uploadBytes } = require("firebase/storage");
-const firebaseConfigObj = require("../config/firebase");
 
-const firebaseConfig = firebaseConfigObj;
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+};
 const filename = (file) => {
   const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
   return file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname);
